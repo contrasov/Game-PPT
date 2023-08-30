f0562cf1c9fd5444bb7644fb636fb47ed7256bc8
contrasov <agvdsbs@gmail.com>
2023-08-29 23:21:05 -0300

    ADD: mudanças no middle

--- script.js
+++ script.js
@@ -3,27 +3,25 @@ const cpuLifeElement = document.getElementById('cpu-life');
 const playerCardsElement = document.querySelector('.player-cards');
 const cpuCardsElement = document.querySelector('.cpu-cards');
 const middleSpaceElement = document.querySelector('.middle-space');
+const playedCardsElement = document.querySelector('.played-cards');
 
 let playerLife = 6;
 let cpuLife = 6;
 
 const cardTypes = ['Pedra', 'Papel', 'Tesoura'];
 
-// Função para atualizar a vida do jogador
 function updatePlayerLife() {
     playerLifeElement.textContent = playerLife;
 }
 
-// Função para atualizar a vida da CPU
 function updateCpuLife() {
     cpuLifeElement.textContent = cpuLife;
 }
 
-// Função para criar uma carta
 function createCard(type, number) {
     const card = document.createElement('div');
     card.classList.add('card');
-    card.setAttribute('data-type', type); // Adicione o atributo data-type à carta
+    card.setAttribute('data-type', type);
     card.innerHTML = `<img src="./src/cartas/${type} ${number}.png" alt="Carta ${type} ${number}" style="width: 80px; height: 120px;">`;
     return card;
 }
@@ -35,13 +33,12 @@ function createCpuCard() {
     return card;
 }
 
-// Função para escolher 6 cartas aleatórias
 function chooseRandomCards() {
     const chosenCards = [];
     
     while (chosenCards.length < 6) {
         const randomType = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-        const randomNumber = Math.floor(Math.random() * 5) + 1; // Cartas de 1 a 5
+        const randomNumber = Math.floor(Math.random() * 5) + 1;
         const cardId = `${randomType} ${randomNumber}`;
 
         if (!chosenCards.includes(cardId)) {
@@ -52,35 +49,6 @@ function chooseRandomCards() {
     }
 }
 
-// Função para jogar uma rodada
-function playRound(playerCard) {
-    // Escolher uma carta aleatória para a CPU
-    const cpuCardTypeRound = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-    const cpuCardNumber = Math.floor(Math.random() * 5) + 1;
-    const cpuCard = createCard(cpuCardTypeRound, cpuCardNumber);
-    
-    middleSpaceElement.innerHTML = ''; // Limpar o espaço do meio
-    middleSpaceElement.appendChild(cpuCard); // Mover a carta da CPU para o meio
-    middleSpaceElement.appendChild(playerCard); // Mover a carta do jogador para o meio
-
-    // Comparar as cartas e determinar o vencedor
-    const playerCardType = playerCard.getAttribute('data-type');
-    const cpuCardType = cpuCard.getAttribute('data-type');
-    const result = compareCards(playerCardType, cpuCardType);
-
-    // Atualizar as vidas do jogador e da CPU com base no resultado
-    if (result === 'player') {
-        cpuLife--;
-    } else if (result === 'cpu') {
-        playerLife--;
-    }
-    
-    updatePlayerLife();
-    updateCpuLife();
-
-}
-
-// Função para comparar as cartas e determinar o vencedor
 function compareCards(playerCardType, cpuCardType) {
     if (playerCardType === cpuCardType) {
         return 'draw';
@@ -95,31 +63,57 @@ function compareCards(playerCardType, cpuCardType) {
     }
 }
 
-// Inicializar o jogo
+function playRound(playerCard) {
+    const cpuCardTypeRound = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    const cpuCardNumber = Math.floor(Math.random() * 5) + 1;
+    const cpuCard = createCard(cpuCardTypeRound, cpuCardNumber);
+
+    middleSpaceElement.innerHTML = '';
+    middleSpaceElement.appendChild(cpuCard);
+    middleSpaceElement.appendChild(playerCard);
+
+    const playerCardType = playerCard.getAttribute('data-type');
+    const cpuCardType = cpuCard.getAttribute('data-type');
+    const result = compareCards(playerCardType, cpuCardType);
+
+    if (result === 'player') {
+        cpuLife--;
+    } else if (result === 'cpu') {
+        playerLife--;
+    }
+    
+    updatePlayerLife();
+    updateCpuLife();
+
+    // Adicionar a carta jogada à área de cartas jogadas
+    addPlayedCard(playerCardType, playerCard.getAttribute('data-number'));
+}
+
+function addPlayedCard(type, number) {
+    const playedCard = createCard(type, number);
+    playedCardsElement.appendChild(playedCard);
+}
+
 function initGame() {
     chooseRandomCards();
     updatePlayerLife();
     updateCpuLife();
 
-    // Criar 6 cartas aleatórias para a CPU
     for (let i = 0; i < 6; i++) {
         const card = createCpuCard();
         cpuCardsElement.appendChild(card);
     }
 
-
-    // Adicione eventos de clique às cartas do jogador
     playerCardsElement.addEventListener('click', (event) => {
         const clickedCard = event.target.closest('.card');
         if (clickedCard && clickedCard.parentElement === playerCardsElement) {
-            playRound(clickedCard); // Jogar a rodada
+            playRound(clickedCard);
             const cpuCards = cpuCardsElement.querySelectorAll('.card');
             if (cpuCards.length > 0) {
-                cpuCardsElement.removeChild(cpuCards[0]); // Remover a primeira carta da CPU
+                cpuCardsElement.removeChild(cpuCards[0]);
             }
         }
     });
 }
 
-// Chamar a função de inicialização do jogo
 initGame();
